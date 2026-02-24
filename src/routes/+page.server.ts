85189529a925f90d97b890dfce77ee23c31915d6
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-24 09:40:17 -0500

    Catch exceptions when getting connections words and use fallback

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,8 +2,15 @@ import { fetchConnections } from "$lib/connections.js";
 
 export const load = async ({ url }) => {
   const date = url.searchParams.get('date');
+  let connections: string[] = [];
+  try {
+    connections = await fetchConnections(date ?? new Date());
+  }
+  catch (e) {
+    connections = ["CONNECTIONS", "WORDS", "WERE", "NOT", "FOUND", "FOR", "SELECTED", "DATE", "PLEASE", "TRY", "ANOTHER", "ONE", "OR", "CHECK", "BACK", "LATER"];
+  }
   return {
     date: date ?? new Date().toISOString().slice(0, 10),
-    connections: await fetchConnections(date ?? new Date())
+    connections
   }
 }
