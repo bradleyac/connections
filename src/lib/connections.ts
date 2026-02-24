d8a4d6637db183d7ae8858d990085fb2d20b4f7d
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-24 09:42:49 -0500

    Empty dates default to today

--- src/lib/connections.ts
+++ src/lib/connections.ts
@@ -1,7 +1,5 @@
-export async function fetchConnections(date: Date | string): Promise<string[]> {
-  let formatted = date instanceof Date ? date.toISOString().slice(0, 10) : date;
-
-  const connectionsUrl = `https://www.nytimes.com/svc/connections/v2/${formatted}.json`;
+export async function fetchConnections(date: string): Promise<string[]> {
+  const connectionsUrl = `https://www.nytimes.com/svc/connections/v2/${date}.json`;
   const json = await (await fetch(connectionsUrl, { method: 'GET', referrer: '' })).json();
 
   // TODO: This only works for strings. Sometimes they have SVGs.
--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,16 +1,16 @@
 import { fetchConnections } from "$lib/connections.js";
 
 export const load = async ({ url }) => {
-  const date = url.searchParams.get('date');
+  const date = url.searchParams.get('date') || new Date().toISOString().slice(0, 10);
   let connections: string[] = [];
   try {
-    connections = await fetchConnections(date ?? new Date());
+    connections = await fetchConnections(date);
   }
   catch (e) {
     connections = ["CONNECTIONS", "WORDS", "WERE", "NOT", "FOUND", "FOR", "SELECTED", "DATE", "PLEASE", "TRY", "ANOTHER", "ONE", "OR", "CHECK", "BACK", "LATER"];
   }
   return {
-    date: date ?? new Date().toISOString().slice(0, 10),
+    date,
     connections
   }
 }
