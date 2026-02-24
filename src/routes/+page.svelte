1907c2fb0115fe91e640ccefcb41c49fce2849e8
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-24 13:40:50 -0500

    Allow swapping colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import PopoverMenuButton from '$lib/components/PopoverMenuButton.svelte';
 	import { flip } from 'svelte/animate';
 
 	let { data } = $props();
 	type category = 'yellow' | 'green' | 'blue' | 'purple' | 'white';
+	const categories = ['purple', 'blue', 'green', 'yellow', 'white'] as category[];
 	let colors = $state({} as Record<string, category>);
 	let colorCounts = $derived.by(() => {
 		const counts: Record<category, number> = {
@@ -41,6 +43,18 @@
 			category
 		>;
 	});
+
+	function swapColors(color1: category, color2: category) {
+		for (const word of Object.keys(colors)) {
+			if (color1 !== 'white' && colors[word] === color1) {
+				colors[word] = color2;
+			} else if (color2 !== 'white' && colors[word] === color2) {
+				colors[word] = color1;
+			}
+		}
+	}
+
+	let opened = $state<string | null>(null);
 </script>
 
 <svelte:boundary>
@@ -66,19 +80,66 @@
 				class="rounded bg-gray-100 bg-gray-200 px-4 py-2 dark:bg-gray-800">Clear</button
 			>
 			{#each Object.entries(colorCounts).filter(([color, count]) => color !== 'white') as [color, count]}
-				<div
+				<PopoverMenuButton
+					bind:opened
+					id={color}
 					class={[
-						'relative flex aspect-square h-full items-center rounded p-5 font-mono',
+						'relative aspect-square h-full content-center rounded px-5 font-mono',
 						color === 'purple' && 'bg-purple-500',
 						color === 'blue' && 'bg-blue-500',
 						color === 'green' && 'bg-green-500',
 						color === 'yellow' && 'bg-yellow-500'
-					]}
+					] as string[]}
+					title={`${count} ${color} connections`}
 				>
+					{#snippet menu(close)}
+						<div class="flex gap-2 rounded bg-gray-300 p-3 shadow-xs/50 dark:bg-gray-700">
+							{#each categories.filter((c) => c !== color) as category}
+								<button
+									onclick={() => {
+										swapColors(color as category, category);
+										close();
+									}}
+									title={category === 'white' ? `Clear ${color}` : `Swap ${color} with ${category}`}
+									class={[
+										'flex aspect-square h-10 place-content-center place-items-center rounded',
+										category === 'purple' && 'bg-purple-500',
+										category === 'blue' && 'bg-blue-500',
+										category === 'green' && 'bg-green-500',
+										category === 'yellow' && 'bg-yellow-500',
+										category === 'white' && 'outline'
+									]}
+								>
+									<span class={['block', category === 'white' && 'hidden']}
+										>{colorCounts[category]}</span
+									>
+									<svg
+										class={[category !== 'white' && 'hidden']}
+										fill="currentColor"
+										viewBox="0 0 256 256"
+										id="Flat"
+										xmlns="http://www.w3.org/2000/svg"
+										><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g
+											id="SVGRepo_tracerCarrier"
+											stroke-linecap="round"
+											stroke-linejoin="round"
+										></g><g id="SVGRepo_iconCarrier">
+											<path
+												d="M202.82861,197.17188a3.99991,3.99991,0,1,1-5.65722,5.65624L128,133.65723,58.82861,202.82812a3.99991,3.99991,0,0,1-5.65722-5.65624L122.343,128,53.17139,58.82812a3.99991,3.99991,0,0,1,5.65722-5.65624L128,122.34277l69.17139-69.17089a3.99991,3.99991,0,0,1,5.65722,5.65624L133.657,128Z"
+											></path>
+										</g></svg
+									>
+								</button>
+							{/each}
+						</div>
+					{/snippet}
+
 					{count}
 					<button
-						onclick={() =>
-							ofColors([color as category]).forEach((word) => (colors[word] = 'white'))}
+						onclick={(e) => {
+							e.stopPropagation();
+							ofColors([color as category]).forEach((word) => (colors[word] = 'white'));
+						}}
 						title={`Clear ${color}`}
 						class={[
 							'text-mono invisible absolute -top-1 -right-1 z-1 h-[22px] w-[22px] rounded rounded-full bg-gray-200 p-1 text-xs outline dark:bg-gray-800',
@@ -100,7 +161,7 @@
 							</g></svg
 						></button
 					>
-				</div>
+				</PopoverMenuButton>
 			{/each}
 		</div>
 		<div class="grid aspect-square w-full max-w-[25rem] grid-cols-4 grid-rows-4 gap-2">
