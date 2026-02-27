14992dbca24507bb049b19e55d6da9ebf1cef6e4
Andrew Bradley <andrew.charles.bradley@gmail.com>
2026-02-27 15:21:21 -0500

    Don't log the connections result

--- src/lib/connections.ts
+++ src/lib/connections.ts
@@ -14,6 +14,5 @@ export async function fetchConnections(date: string): Promise<ConnectionsCard[]>
     .filter(Boolean)]
     .toSorted((a, b) => a.position - b.position);
 
-  console.log(result);
   return result;
 }
